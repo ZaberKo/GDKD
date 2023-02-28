6433a2a7dc0d477a7a1311c307049c62656d4e42
zaber <20830726+ZaberKo@users.noreply.github.com>
2023-03-01 00:03:43 +0800

    add more constraint on beta

--- mdistiller/distillers/utils.py
+++ mdistiller/distillers/utils.py
@@ -4,19 +4,21 @@ import torch.nn.functional as F
 def kl_div(log_p, log_q,  T, kl_type, reduction="batchmean"):
     if kl_type == "forward":
         res = F.kl_div(log_p, log_q, reduction=reduction,
-                       log_target=True) * (T**2)
+                       log_target=True) 
     elif kl_type == "reverse":
         res = F.kl_div(log_q, log_p, reduction=reduction,
-                       log_target=True) * (T**2)
+                       log_target=True)
     elif kl_type == "both":
         res = 0.5 * (
             F.kl_div(log_p, log_q, reduction=reduction, log_target=True) +
             F.kl_div(log_q, log_p, reduction=reduction, log_target=True)
-        ) * (T**2)
+        )
     else:
         raise ValueError(f"Unknown kl_type: {kl_type}")
     
     if reduction=="none":
         res = res.sum(dim=1) # [B,C]->[B]
+
+    res = res * (T**2)
 
     return res
