5a4096932f4ee9f246ebea8624ba09236f613fb7
zaber <20830726+ZaberKo@users.noreply.github.com>
2023-09-21 18:40:42 +0800

    add calc_coco_teacher_logits

--- detection/test.py
+++ detection/test.py
@@ -1,3 +1,31 @@
+# %%
 import torch
 
-data=torch.load("pretrained/r18-r101.pth")
+# %%
+# data=torch.load("pretrained/r18-r101.pth")
+data = torch.load("output_final/DKD-R18-R101_temjbxn9/model_0004999.pth")
+
+print(data)
+# %%
+import torch
+from pathlib import Path
+
+def convert(path):
+    state_dict = torch.load(path)
+    new_ckpt = {}
+    for k, v in state_dict.items():
+        
+        if "teacher" == k.split('.')[0]:
+            print(k)
+            new_ckpt[k] = v
+        else:
+            print(f"{k} -> student.{k}")
+            new_ckpt["student."+k] = v
+
+    return new_ckpt
+
+root=Path("pretrained")
+for path in Path("pretrained_old").glob("*.pth"):
+    new_ckpt = convert(path)
+    torch.save(new_ckpt, root/path.name)
+# %%
